de66822cd25b6793688ee9361f24b4117778681c
= <=>
2014-04-10 23:37:53 -0300

    adicionando modpow

---
+++ cpp/matematica/teoria_dos_numeros/modpow.cpp
@@ -0,0 +1,13 @@
+ll pot(ll a, ll b, ll MOD) {
+	ll x = 1, y = a;
+	while(b > 0) {
+		if(b%2 == 1) {
+			x=(x*y);
+			if(x>MOD) x%=MOD;
+		}
+		y = (y*y);
+		if(y>MOD) y%=MOD;
+		b /= 2;
+	}
+	return x;
+}
