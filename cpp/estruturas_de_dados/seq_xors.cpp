54023674559b3e17b0777dfc014cddc3da6c9a03
Caio Oliveira <caio@caio-note-algar.(none)>
2014-04-25 09:40:34 -0300

    Outra forma

--- cpp/estruturas_de_dados/seq_xors.cpp
+++ cpp/estruturas_de_dados/seq_xors.cpp
@@ -1,1 +1,11 @@
 int ans = N & (N % 2 ? 0 : ~0) | ( ((N & 2)>>1) ^ (N & 1) );
+
+// Outra forma
+int xorUpToK(int k) {
+	switch (k % 4) {
+		case 0: return k;
+		case 1: return 1;
+		case 2: return k + 1;
+		case 3: return 0;
+	}
+}
