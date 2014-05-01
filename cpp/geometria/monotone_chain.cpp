3905a04745ee98de224c8d91fff03258b09c77f1
= <=>
2014-05-01 15:19:23 -0300

    adicionando monotone chain

---
+++ cpp/geometria/monotone_chain.cpp
@@ -0,0 +1,18 @@
+
+set<point> pts;
+vector<point> up, dn;
+void convexHull(){
+	up.assign(pts.size(),point());
+	dn.assign(pts.size(),point());
+	int i = 0, j = 0;
+	
+	for(set<point>::iterator it = pts.begin(); it != pts.end(); ++it){
+		while(i > 1 && orientation(up[i-2], up[i-1], *it) <= 0) i--;
+		while(j > 1 && orientation(dn[j-2], dn[j-1], *it) >= 0) j--;
+		
+		up[i++] = *it;
+		dn[j++] = *it;
+	}
+	up.resize(i);
+	dn.resize(j);
+}
