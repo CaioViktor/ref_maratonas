98d458d8a0f6bd1547ef7bfebe520847bf55ad21
= <=>
2014-08-20 18:47:19 -0300

    implementacao mais rapida de fft

--- cpp/matematica/calculo/fft.cpp
+++ cpp/matematica/calculo/fft.cpp
@@ -1,64 +1,55 @@
-typedef complex<double> cd; // possivel otimizacao: usar struct no lugar
-typedef vector<cd> vcd;
+typedef long double ld;
+typedef complex<ld> base;
 
-vcd fft(const vcd &as) {
-  int n = as.size();
-  int k = 0;
-  while ((1 << k) < n) k++;
-  vector<int> rev(n);
-  rev[0] = 0;
-  int high1 = -1;
-  for (int i = 1; i < n; i++) {
-    if ((i & (i - 1)) == 0)
-      high1++;
-    rev[i] = rev[i ^ (1 << high1)];
-    rev[i] |= (1 << (k - high1 - 1));
-  }
+const double PI = acos(-1.0);
 
-  vcd roots(n);
-  for (int i = 0; i < n; i++) {
-    double alpha = 2 * M_PI * i / n;
-    roots[i] = cd(cos(alpha), sin(alpha));
-  }
+void fft (vector<base> & a, bool invert)
+{
+	ll n = (int) a.size();
 
-  vcd cur(n);
-  for (int i = 0; i < n; i++)
-    cur[i] = as[rev[i]];
+	for (int i=1, j=0; i<n; ++i) {
+		int bit = n >> 1;
+		for (; j>=bit; bit>>=1)
+			j -= bit;
+		j += bit;
+		if (i < j)
+			swap (a[i], a[j]);
+	}
 
-  for (int len = 1; len < n; len <<= 1) {
-    vcd ncur(n);
-    int rstep = roots.size() / (len * 2);
-    for (int pdest = 0; pdest < n;) {
-      int p1 = pdest;
-      for (int i = 0; i < len; i++) {
-        cd val = roots[i * rstep] * cur[p1 + len];
-        ncur[pdest] = cur[p1] + val;
-        ncur[pdest + len] = cur[p1] - val;
-        pdest++, p1++;
-      }
-      pdest += len;
-    }
-    cur.swap(ncur);
-  }
-  return cur;
+	for (int len=2; len<=n; len<<=1) {
+		ld ang = 2*PI/len * (invert ? -1 : 1);
+		base wlen (cos(ang), sin(ang));
+		for (int i=0; i<n; i+=len) {
+			base w (1);
+			for (int j=0; j<len/2; ++j) {
+				base u = a[i+j],  v = a[i+j+len/2] * w;
+				a[i+j] = u + v;
+				a[i+j+len/2] = u - v;
+				w *= wlen;
+			}
+		}
+	}
+	if (invert)
+		for (int i=0; i<n; ++i)
+			a[i] /= n;
 }
 
 
-vcd fft_rev(const vcd &as) {
-  vcd res = fft(as);
-  for (int i = 0; i < (int)res.size(); i++) res[i] /= as.size();
-  reverse(res.begin() + 1, res.end());
-  return res;
+// Exemplo de utilizacao: multiplicacao de dois polinomios A e B em O(n log n)
+void multiply (const vector<ll> & a, const vector<ll> & b, vector<ll> & res)
+{
+	vector<base> fa (a.begin(), a.end()),  fb (b.begin(), b.end());
+	size_t n = 1;
+	// n deve ser a menor potencia de 2 >= grau dos polinomios
+	while (n < max (a.size(), b.size()))  n <<= 1;
+	n <<= 1; // multiplica por 2 (G(P*Q) = G(P)+G(Q))
+	fa.resize (n),  fb.resize (n);
+
+	fft (fa, false),  fft (fb, false); // FFT dos polinomios
+	for (size_t i=0; i<n; ++i)
+		fa[i] *= fb[i];
+	fft (fa, true); // FFT inverso da multiplicacao
+	res.resize (n);
+	for (size_t i=0; i<n; ++i)
+		res[i] = ll (fa[i].real() + 0.5);
 }
-
-// utilizacao:
-int n; // grau dos polinomios A e B
-vcd A(2*n, 0), B(2*n, 0); // pols a serem multiplicados
-
-// le A e B
-
-vcd FA = fft(A), FB = fft(B), FC; // fourier
-for(int i = 0; i < 2*N; i++){
-  FC[i] = FA[i]*FB[i];
-}
-vcd C = fft_rev(C); // resultado da multiplicacao
