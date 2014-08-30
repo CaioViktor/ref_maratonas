1bd5d1a7217eaddb71319e2219e0a6d799b88745
= <=>
2014-08-30 01:00:49 -0300

    arrumando sort por angulo (deixando mais rapido)

--- cpp/geometria/aleatorio/angle_sort.cpp
+++ cpp/geometria/aleatorio/angle_sort.cpp
@@ -1,17 +1,14 @@
 // Cmp para sort em sentido anti horario
-bool cmp(point lhs, point lhs){
-	int qR = quadrant(seeker, rhs),
-		qL = quadrant(seeker, lhs);
-
+bool cmp(point lhs, point rhs){
 	// Comparacao pelo quadrante primeiro garante a condicao de
 	// transitividade do sort (se a > b e b > c , a > c)
-	if(qR < qL) return true;
-	if(qR > qL) return false;
+	if(lhs.y < pivot.y  && rhs.y >= pivot.y) return true;
+	if(lhs.y >= pivot.y && rhs.y <  pivot.y) return false;
 	
-	if(ccw(seeker, lhs.p, p)) return false;
-	if(ccw(seeker, p, lhs.p)) return true;
+	if(cross(toVec(pivot,lhs),toVec(pivot,rhs)) > 0) return true;
+	if(cross(toVec(pivot,lhs),toVec(pivot,rhs)) < 0) return false;
 	
 	// Espaco normalmente populado por condicoes do sweep
 	
-	return true;
+	return false;
 }
