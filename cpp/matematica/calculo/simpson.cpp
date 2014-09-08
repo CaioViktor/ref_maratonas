6f6a1a8f8a1fc41af043c0ac63b8cc5a0df6ed74
Caio <caio@caio-note-algar.(none)>
2014-09-08 16:35:07 -0300

    adicionando template pra usar struct como func

--- cpp/matematica/calculo/simpson.cpp
+++ cpp/matematica/calculo/simpson.cpp
@@ -1,10 +1,12 @@
-double simps(double a, double b) {
+template<typename T>
+double simps(double a, double b, T f) {
   return (f(a) + 4*f((a+b)/2) + f(b))*(b-a)/6;
 }
 
-double integrate(double a, double b){
+template<typename T>
+double integrate(double a, double b, T f){
   double m = (a+b)/2;
-  double l = simps(a,m), r = simps(m,b), tot=simps(a,b);
+  double l = simps(a,m, f), r = simps(m,b, f), tot=simps(a,b, f);
   if (fabs(l+r-tot) < eps) return tot;
-  return integrate(a,m)+integrate(m,b);
+  return integrate(a,m,f)+integrate(m,b,f);
 }
