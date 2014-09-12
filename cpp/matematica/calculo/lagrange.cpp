0a77c4e3507ff97e10adf985a9dc881759b22b50
Caio <caioaao@gmail.com>
2014-09-12 11:24:55 -0300

    arrumando lagrange

--- cpp/matematica/calculo/lagrange.cpp
+++ cpp/matematica/calculo/lagrange.cpp
@@ -2,7 +2,7 @@ double lagrange(double p, vector<double> &x, vector<double> &y){
   double num, den, ans = 0;
   for(size_t i = 0; i < x.size(); i++){
     num = den = 1.0;
-    for(size_t j = 0; j < x.size(); j++){
+    for(size_t j = 0; j < x.size(); j++) if(i != j){
       num *= (p-x[j]);
       den *= (x[i]-x[j]);
     }
