9708c3f60e5b06929a88f03ad97efcb09a3ba7e8
Caio <caioaao@gmail.com>
2014-09-12 11:37:58 -0300

    simplificando

--- cpp/geometria/basico/quadrante.cpp
+++ cpp/geometria/basico/quadrante.cpp
@@ -1,13 +1,11 @@
+// pontos em cima do eixo: pertencem ao quadrante de maior indice
 int quadrant(point o, point a){
-	if(cross(toVec(o,a),point( 1, 0)) == 0)
-		return (ccw(o, o + ay, a) ? 2 : 0);
-	if(cross(toVec(o,a),point( 0, 1)) == 0)
-		return (ccw(o, o + ax, a) ? 1 : 3);
-	bool rAx = ccw(o, o + ax, a),
-		 rAy = ccw(o, o + ay, a);
-	
-	if(!rAx && !rAy)	  return 3;
-	else if(!rAx &&  rAy) return 2;
-	else if( rAx && !rAy) return 0;
-	else				  return 1;
+  int x = a.x - o.x, y = a.y - o.y;
+  if(x == 0 && y == 0) return 0;
+  
+  if(x >  0 && y >= 0) return 0;
+  if(x <= 0 && y >  0) return 1;
+  if(x <  0 && y <= 0) return 2;
+  
+  return 3;
 }
