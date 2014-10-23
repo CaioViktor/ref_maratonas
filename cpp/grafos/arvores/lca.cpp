dd7de86724de83fb0952249d52b6f656a62d9066
alv-r- <no>
2014-10-23 18:13:10 -0200

    ?

--- cpp/grafos/arvores/lca.cpp
+++ cpp/grafos/arvores/lca.cpp
@@ -1,6 +1,28 @@
+class RMQ {
+private:
+  int _A[2*MAXN], SpT[2*MAXN][2*LOGTWON];
+public:
+  void build(int n, int A[]) {
+    for (int i = 0; i < n; i++) { _A[i] = A[i]; SpT[i][0] = i; }
+    
+    for (int j = 1; (1<<j) <= n; j++)
+      for (int i = 0; i + (1<<j) - 1 < n; i++)
+        if (_A[SpT[i][j-1]] < _A[SpT[i+(1<<(j-1))][j-1]])
+          SpT[i][j] = SpT[i][j-1];
+        else
+          SpT[i][j] = SpT[i+(1<<(j-1))][j-1];
+  }
+
+  int query(int i, int j) {
+    int k = (int)floor(log((double)j-i+1) / log(2.0));
+    if (_A[SpT[i][k]] <= _A[SpT[j-(1<<k)+1][k]]) return SpT[i][k];
+    else return SpT[j-(1<<k)+1][k];
+} };
+
 class LCA{
 private:
-  int L[2*MAXN], E[2*MAXN], H[MAXN], idx, st[8*MAXN], n;
+  RMQ rmq;
+  int L[2*MAXN], E[2*MAXN], H[MAXN], idx;
 
   void dfs(int cur, int depth, vvi & children) {
     H[cur] = idx;
@@ -12,39 +34,15 @@ private:
       L[idx++] = depth;
     }
   }
-  void build_tree(int x, int l, int r, vvi & children){
-    if(l == r)
-      st[x] = l;
-    else{
-      build_tree(cl(x), l, (l+r)/2, children);
-      build_tree(cr(x), (l+r)/2+1,r, children);
-      if(L[st[cl(x)]] < L[st[cr(x)]]) st[x] = st[cl(x)];
-      else st[x] = st[cr(x)];
-    }
-  }
-  
-  int query_tree(int x, int l, int r, int i, int j){
-    if(i > r || j < l) return -1;
-    if(l >= i && r <= j) return st[x];
-    
-    int vl = query_tree(cl(x), l, (l+r)/2, i, j),
-      vr = query_tree(cr(x), (l+r)/2+1, r, i, j);
-    
-    if(vl == -1) return vr;
-    if(vr == -1) return vl;
-    if(L[vl] < L[vr]) return vl;
-    return vr;
-  }
 public:
   void build(vvi & children){
     idx = 0;
-    n = idx - 1;
     memset(H, -1, sizeof H);
     dfs(0, 0, children);
-    build_tree(1,0, n, children);
+    rmq.build(idx,L);
   }
   int query(int u, int v){
     if(H[v] < H[u])swap(u,v);
-    return E[query_tree(1,0,n,H[u],H[v])];
+    return E[rmq.query(H[u],H[v])];
   }
 };
